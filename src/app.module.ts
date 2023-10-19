d8967cedb870e04f95bdfc6fe36eb33c1726b3be
brenowillians <brenownmachado@gmail.com>
2023-10-19 20:11:01 -0300

    implementacao .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ComprasMercado } from './entities/compras-mercado.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      expandVariables: true,
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      database: 'learn',
-      username: 'postgres',
-      password: '123456',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       autoLoadEntities: true,
       logging: true,
